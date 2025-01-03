eef9be228908ec1763ee3042fdd968d1db31bc59
msribeiro2010 <msribeiro2010@icloud.com>
2025-01-03 19:56:35 -0300

    Corrigido erro de template tag e melhorado layout do dashboard
    - Removida dependência do template tag personalizado
    - Atualizado layout do dashboard para melhor organização
    - Melhorada exibição de conflitos usando filtros built-in

--- funcionarios/apps.py
+++ funcionarios/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class FuncionariosConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "funcionarios"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'funcionarios'
+    verbose_name = 'Controle de Funcionários'
+
+    def ready(self):
+        # Registra os template tags
+        from django.template import base
+        base.add_to_builtins('django.templatetags.i18n')
+        base.autodiscover_modules('templatetags')
