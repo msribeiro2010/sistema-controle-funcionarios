9265c725fc65d45fb2cbe05996e07400f2df14d0
msribeiro2010 <msribeiro2010@icloud.com>
2025-01-11 10:54:44 -0300

    Initial commit

--- core/settings.py
+++ core/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('controle_funcionariosE'),
-        'USER': config('postgres'),
-        'PASSWORD': config('mar'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='5432'),
     }
