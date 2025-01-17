2069e88a33f56f413887038391a986bebe358a4d
msribeiro2010 <msribeiro2010@icloud.com>
2025-01-17 20:21:50 -0300

    Corrige comando de inicialização

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Criar e ativar ambiente virtual
+python -m venv .venv
+source .venv/bin/activate
+
 # Instalar dependências
 python -m pip install --upgrade pip
 pip install -r requirements.txt
@@ -13,5 +17,8 @@ python manage.py collectstatic --noinput
 
 # Executar migrações
 python manage.py migrate --noinput
+
+# Verificar instalação do gunicorn
+which gunicorn
 
 echo "Build completed successfully!" 
