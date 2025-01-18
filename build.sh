36efcb2d994c1a72640fc51be70e8c214c7524f6
msribeiro2010 <msribeiro2010@icloud.com>
2025-01-17 22:00:09 -0300

    Simplifica configuração de inicialização

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Criar ambiente virtual
+python -m venv .venv
+source .venv/bin/activate
+
 # Instalar dependências
 python -m pip install --upgrade pip
 pip install -r requirements.txt
@@ -13,9 +17,6 @@ python manage.py collectstatic --noinput
 
 # Executar migrações
 python manage.py migrate --noinput
-
-# Dar permissão ao script de inicialização
-chmod +x start.sh
 
 # Verificar instalação
 echo "Python path: $(which python)"
